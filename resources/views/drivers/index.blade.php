967155447deb3635b747743c2b60905eafa909cb
Jerry <azhagirishnakar5@gmail.com>
2026-02-09 15:50:59 +0530

    #9  drivers fixed

--- resources/views/drivers/index.blade.php
+++ resources/views/drivers/index.blade.php
@@ -58,6 +58,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="table-list">
                 <div class="row">
                     <div class="col-12">
